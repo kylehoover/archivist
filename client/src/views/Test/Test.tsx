6381371b63cd3a5efda53f5f6f6203ecc0bfb8e0
Kyle Hoover <khoov22@gmail.com>
2020-01-11 22:12:10 -0600

    Add invalid style for Input

--- client/src/views/Test/Test.tsx
+++ client/src/views/Test/Test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Button from '../../components/Button'
 import Container from '../../components/Container'
 import Input from '../../components/Input'
 import Panel from '../../components/Panel'
@@ -7,17 +8,19 @@ import Panel from '../../components/Panel'
 const Test = () => {
   return (
     <div className='Test'>
-      <Container centered>
+      <Container maxWidth={40} centered>
         <Panel title='Forms' color='blue'>
-          <div style={{marginBottom: 32}}>
-            <Input label='Name' name='name' labelPosition='embedded' />
-          </div>
-          <div style={{marginBottom: 32}}>
-            <Input label='Name' name='name' labelPosition='top' />
-          </div>
-          <div style={{marginBottom: 32}}>
-            <Input label='Name' name='name' labelPosition='left' />
-          </div>
+          <form>
+            <div style={{marginBottom: 32}}>
+              <Input label='Name' name='name' labelPosition='embedded' />
+            </div>
+            <div style={{marginBottom: 32}}>
+              <Input label='Name' name='name' labelPosition='top' />
+            </div>
+            <div style={{marginBottom: 32}}>
+              <Input label='Name' name='name' labelPosition='left' />
+            </div>
+          </form>
         </Panel>
       </Container>
     </div>
